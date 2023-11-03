bfb3c99557911a8e75970ba248bbcd0968423c03
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2023-11-02 21:38:10 -0500

    struct

---
+++ ControladorReservas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct comando
+{
+    string comando_f = "-f";
+    string comando_c = "-c";
+    string comando_a = "-a";
+    string comando_n = "-n";
+    string comando_p = "-p";
+};
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
