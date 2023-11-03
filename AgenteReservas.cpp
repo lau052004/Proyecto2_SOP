f9e6f2a9da928079244cf3aa7b39a857b9dd1296
joseManuel1234 <josemrodriguez@javeriana.edu.co>
2023-11-02 21:36:31 -0500

    bobi

--- AgenteReservas.cpp
+++ AgenteReservas.cpp
@@ -1,1 +1,51 @@
-// $ ./controlador –i horaInicio –f horafinal –s segundoshora –t totalpersonas –p pipecrecibe
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+struct Registro {
+    string nombre;
+    int hora;
+    int personas;
+};
+
+void leerArchivo(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo." << endl;
+        return;
+    }
+
+    string linea;
+    Registro registro;
+
+    while (getline(archivo, linea)) {
+        istringstream ss(linea);
+        string token;
+
+        if (getline(ss, token, ',')) {
+            registro.nombre = token;
+        }
+
+        if (getline(ss, token, ',')) {
+            registro.hora = stoi(token);
+        }
+
+        if (getline(ss, token, ',')) {
+            registro.personas = stoi(token);
+        }
+
+        cout << "Nombre: " << registro.nombre << ", Hora: " << registro.hora << ", Personas: " << registro.personas << endl;
+    }
+
+    archivo.close();
+}
+
+int main() {
+    const string nombreArchivo = "datos.txt"; 
+    leerArchivo(nombreArchivo);
+    return 0;
+}
